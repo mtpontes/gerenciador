9c799e5ebc0633be1172b52bf268d4f03f3ba91b
Mateus <mateuspsdd@gmail.com>
2024-01-23 10:49:37 -0300

    Corrige a alternância do botão `arquivar`

--- src/main/webapp/js/view/usuarioEmpresas/criaColecaoNovosElementosLi.js
+++ src/main/webapp/js/view/usuarioEmpresas/criaColecaoNovosElementosLi.js
@@ -1,67 +1,79 @@
 export function criaColecaoNovosElementosLi(empresas) {
 	const colecaoNovosLi  = [];
 	empresas.forEach(empresa => {
+		//form .lista
 	    const novoFormulario = document.createElement('form');
 	    novoFormulario.classList.add('lista');
 	    novoFormulario.id = empresa.id;
-	
+	    novoFormulario.dataset.ativo = empresa.ativo;
+	    
+	    
+		//div .lista-nome-data
 	    const divNomeData = document.createElement('div');
 	    divNomeData.classList.add('lista-nome-data');
-	
+		//p .lista-nome
 	    const paragrafoNome = document.createElement('p');
 	    paragrafoNome.classList.add('lista-nome');
 	    paragrafoNome.id = 'lista-nome';
 	    paragrafoNome.textContent = empresa.base.nome;
-	
+		//p .lista-data
 	    const paragrafoData = document.createElement('p');
 	    paragrafoData.classList.add('lista-data');
 	    paragrafoData.id = 'lista-data';
 	    paragrafoData.textContent = empresa.base.data;
-	
+		//input .entrada-nome
 	    const inputNome = document.createElement('input');
 	    inputNome.classList.add('entrada-nome', 'lista-nome');
 	    inputNome.style.display = 'none';
-	
+		//input .entrada-data
 	    const inputData = document.createElement('input');
 	    inputData.classList.add('entrada-data', 'lista-data');
 	    inputData.style.display = 'none';
+	    
 	
+		//div .container-edita-arquiva
 	    const divEditaArquiva = document.createElement('div');
 	    divEditaArquiva.classList.add('container-edita-arquiva');
-	
+	    
+		//a .botao-editar
 	    const linkEditar = document.createElement('a');
 	    linkEditar.classList.add('botao-editar');
 	    linkEditar.id = 'botao-editar';
 	    linkEditar.href = `/gerenciador/empresa?acao=mostraEmpresa&nome=${empresa.base.nome}&id=${empresa.id}&data=${empresa.base.data}`;
-	
+		//span .texto-editar
 	    const spanTextoEditar = document.createElement('span');
 	    spanTextoEditar.classList.add('texto-editar');
 	    spanTextoEditar.textContent = 'Editar';
-	
+		//span .icone-editar
 	    const spanIconeEditar = document.createElement('span');
 	    spanIconeEditar.classList.add('icone-editar', 'material-symbols-outlined');
 	    spanIconeEditar.textContent = 'edit';
-	
+
 	    linkEditar.appendChild(spanTextoEditar);
 	    linkEditar.appendChild(spanIconeEditar);
-	
+		
+		//a .botao-arquivar	
 	    const linkArquivar = document.createElement('a');
 	    linkArquivar.classList.add('botao-arquivar');
 	    linkArquivar.id = 'botao-arquivar';
 	    linkArquivar.dataset.empresaid = empresa.id;
 	    linkArquivar.href = `/gerenciador/empresa?acao=removeEmpresa`;
-	
+		//span .texto-arquiva
 	    const spanTextoArquiva = document.createElement('span');
 	    spanTextoArquiva.classList.add('texto-arquiva');
-	    spanTextoArquiva.textContent = 'Arquivar';
-	
+	    spanTextoArquiva.textContent = (empresa.ativo == false) ? 'Desarquivar' : 'Arquivar';
+		//span .icone-arquiva
 	    const spanIconeArquiva = document.createElement('span');
 	    spanIconeArquiva.classList.add('icone-arquiva', 'material-symbols-outlined');
-	    spanIconeArquiva.textContent = 'archive';
+	    const valor = (empresa.ativo == false) ? 'unarchive' : 'archive';
+	    console.log(empresa.ativo, valor);
+	    spanIconeArquiva.textContent = valor;
 	
 	    linkArquivar.appendChild(spanTextoArquiva);
 	    linkArquivar.appendChild(spanIconeArquiva);
+	    
 	
+		//input .botao-enviar
 	    const botaoEnviar = document.createElement('input');
 	    botaoEnviar.classList.add('botao-enviar');
 	    botaoEnviar.id = 'inputHidden';
