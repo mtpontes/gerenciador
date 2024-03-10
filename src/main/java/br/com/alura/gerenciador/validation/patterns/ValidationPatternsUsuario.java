4d6ee4df99cf053dc5cea4fdbb3710ca30fb5cf8
Mateus Pontes <mateuspsdd@gmail.com>
2024-03-10 17:00:59 -0300

    Update ValidationPatterns error messages

--- src/main/java/br/com/alura/gerenciador/validation/patterns/ValidationPatternsUsuario.java
+++ src/main/java/br/com/alura/gerenciador/validation/patterns/ValidationPatternsUsuario.java
@@ -2,5 +2,5 @@ package br.com.alura.gerenciador.validation.patterns;
 
 public class ValidationPatternsUsuario {
 	public static final String NOME_USUARIO_REGEX_PATTERN = "^[A-Za-zÀ-ÖØ-öø-ÿ' _-]+$";
-	public static final String NOME_USUARIO_ERROR_MESSAGE = "VALIDATION ERROR: Nome só permite letras, apóstrofo (\\') e hífen (-)";
+	public static final String NOME_USUARIO_ERROR_MESSAGE = "Nome só permite letras, apóstrofo (\\') e hífen (-)";
 }
