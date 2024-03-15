dabd3334c1d1b2a6cfae1a44f2d174e7a5d21cb1
Mateus Pontes <mateuspsdd@gmail.com>
2024-03-15 17:08:38 -0300

    ...

--- src/main/java/br/com/gerenciador/util/JPAUtil.java
+++ src/main/java/br/com/gerenciador/util/JPAUtil.java
@@ -5,7 +5,7 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class JPAUtil {
-	private static final EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory("test");
+	private static final EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory("gerenciador");
 	
 	public static EntityManager getEntityManager() {
 		return FACTORY.createEntityManager();
