8e2f282781eef39cab579825cbae821142be0c38
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-29 21:36:19 -0300

    feat: adicionar `PaginationUtil`

---
+++ src/main/java/br/com/gerenciador/util/PaginationUtil.java
@@ -0,0 +1,19 @@
+package br.com.gerenciador.util;
+
+import br.com.gerenciador.pagination.Pagination;
+import br.com.gerenciador.pagination.PaginationBuilder;
+import jakarta.servlet.http.HttpServletRequest;
+
+public class PaginationUtil {
+
+    public static Pagination criaPagination(HttpServletRequest request, Long totalPages) {
+		String paramPageNumber = request.getParameter("page");
+		String paramPageSize = request.getParameter("size");
+		Pagination pg = new PaginationBuilder()
+			.setPageNumber(paramPageNumber)
+			.setPageSize(paramPageSize)
+			.setTotalPages(totalPages)
+			.build();
+		return pg;
+	}
+}
