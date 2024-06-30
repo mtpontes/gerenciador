af6c5daf280a6c04611e179e4a395c402055428f
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-29 21:31:46 -0300

    feat: adicionar `ControllerUsuario`
    - implementação completamente refeita

---
+++ src/main/java/br/com/gerenciador/controller/usuario/ControllerUsuario.java
@@ -0,0 +1,58 @@
+package br.com.gerenciador.controller.usuario;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import br.com.gerenciador.controller.Command;
+import br.com.gerenciador.controller.NotFoundCommand;
+import br.com.gerenciador.controller.usuario.commands.NovoUsuarioCommand;
+import br.com.gerenciador.controller.usuario.commands.LoginFormCommand;
+import br.com.gerenciador.controller.usuario.commands.NovoUsuarioFormCommand;
+import br.com.gerenciador.controller.usuario.commands.LoginCommand;
+import br.com.gerenciador.controller.usuario.commands.LogoutCommand;
+import br.com.gerenciador.controller.usuario.commands.VerificaLoginCommand;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ControllerUsuario extends HttpServlet {
+
+	private final Map<String, Command> postCommands;
+	private final Map<String, Command> getCommands;
+
+	public ControllerUsuario(Map<String, Command> postCommands, Map<String, Command> getCommands) {
+		this.postCommands = postCommands;
+		this.getCommands = getCommands;
+	}
+
+	public ControllerUsuario() {
+		this.postCommands = new HashMap<>();
+		this.postCommands.put("login", new LoginCommand());
+		this.postCommands.put("verificaLogin", new VerificaLoginCommand());
+		this.postCommands.put("novoUsuario", new NovoUsuarioCommand());
+
+		this.getCommands = new HashMap<>();
+		this.getCommands.put("loginForm", new LoginFormCommand());
+		this.getCommands.put("novoUsuarioForm", new NovoUsuarioFormCommand());
+		this.getCommands.put("logout", new LogoutCommand());
+	}
+	
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.getCommand(request, this.postCommands)
+			.executa(request, response);
+	}
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.getCommand(request, this.getCommands)
+			.executa(request, response);
+	}
+
+	private Command getCommand(HttpServletRequest request, Map<String, Command> commands) {
+		String acao = request.getParameter("acao");
+		return commands.getOrDefault(acao, new NotFoundCommand());
+	}
+}
