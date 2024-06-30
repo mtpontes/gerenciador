8f50f0ec79ee4841466ad42f6f53527a6eda91a7
Mateus Pontes <mateuspsdd@gmail.com>
2024-06-29 21:43:14 -0300

    test: mover `EmpresaBuilder` para `gerenciador.integration.config`

--- src/test/java/br/com/gerenciador/integration/config/EmpresaBuilder.java
+++ src/test/java/br/com/gerenciador/integration/config/EmpresaBuilder.java
@@ -1,9 +1,9 @@
-package br.com.gerenciador.integration;
+package br.com.gerenciador.integration.config;
 
-import br.com.gerenciador.dto.empresa.EmpresaBaseDTO;
-import br.com.gerenciador.dto.empresa.request.NovaEmpresaDTO;
 import br.com.gerenciador.modelo.Empresa;
 import br.com.gerenciador.modelo.Usuario;
+import br.com.gerenciador.modelo.dto.empresa.EmpresaBaseDTO;
+import br.com.gerenciador.modelo.dto.empresa.request.NovaEmpresaDTO;
 
 public class EmpresaBuilder {
 	private String nome;
